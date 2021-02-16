08699f2cb5332056a79ff8dc3a2927342978aea0
josef0731 <joseph.chifung.ng@gmail.com>
2021-02-16 15:16:50 +0000

    tidy up again

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -1,49 +1,97 @@
----
-layout: none
----
-
-var store = [
-  {%- for c in site.collections -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
-    {%- for doc in docs -%}
-      {%- if doc.header.teaser -%}
-        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
-      {%- else -%}
-        {%- assign teaser = site.teaser -%}
-      {%- endif -%}
-      {
-        "title": {{ doc.title | jsonify }},
-        "excerpt":
-          {%- if site.search_full_content == true -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | jsonify }},
-          {%- else -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-          {%- endif -%}
-        "categories": {{ doc.categories | jsonify }},
-        "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | absolute_url | jsonify }},
-        "teaser": {{ teaser | absolute_url | jsonify }}
-      }{%- unless forloop.last and l -%},{%- endunless -%}
-    {%- endfor -%}
-  {%- endfor -%}]
+var store = [{
+        "title": "Hello world!",
+        "excerpt":"We have launched our group page today!   Stay tuned for updates :)  ","categories": [],
+        "tags": [],
+        "url": "/hello-world/",
+        "teaser": null
+      },{
+        "title": "TITINdb Application Note Is Published in Bioinformatics",
+        "excerpt":"   Our application note of TITINdb has just been published in Bioinformatics   https://doi.org/10.1093/bioinformatics/btx424   Congratulations to Anna!  ","categories": [],
+        "tags": [],
+        "url": "/titindb/",
+        "teaser": "/assets/images/Screen-Shot-2017-07-04-at-19.40.03.png"
+      },{
+        "title": "New FEBS Journal paper on engineering a photoswitchable mammalian pyruvate kinase",
+        "excerpt":"Our new work on mammalian pyruvate kinase M2 (PKM2) has been recently published on the FEBS Journal. Jamie performed Molecular Dynamics Simulations and analyses to guide the engineering of a photo-switchable PKM2 enzyme. Congratulations to Jamie! Gehrig S, Macpherson JA et al. An engineered photoswitchable mammalian pyruvate kinase. FEBS J...","categories": [],
+        "tags": [],
+        "url": "/jamie-febs/",
+        "teaser": "/assets/images/photoswitchablePKM2image.png"
+      },{
+        "title": "Stay tuned for Systems Immunology Workshop 2018 @ Surrey!",
+        "excerpt":"   Interdisciplinary workshop on tools, modelling and applications of adaptive immune repertoire analysis hosted by our MABRA consortium!   ","categories": [],
+        "tags": [],
+        "url": "/mabra-symposium/",
+        "teaser": "/assets/images/flyer-ppt_v4.png"
+      },{
+        "title": "Book chapter on self-assembly published in RSC",
+        "excerpt":"   Irene and Franca contributed a book chapter on multi-scale modelling of self-assembly, and is published in the book Synthetic Biology (Volume 2) from the Royal Society of Chemistry! Congratulations to Irene! Link: http://pubs.rsc.org/en/content/ebook/978-1-78262-120-1  ","categories": [],
+        "tags": [],
+        "url": "/book-chapter-rsc/",
+        "teaser": "/assets/images/Screen-Shot-2018-01-15-at-10.31.52.png"
+      },{
+        "title": "Irene’s work has been featured on The Times!",
+        "excerpt":"   Anna, Joseph, Sun and Franca contributed a review on the impact of genetic variants on protein-protein interaction networks in the Current Opinion in Structural Biology, and is now published on-line! Link here   ","categories": [],
+        "tags": [],
+        "url": "/curropin/",
+        "teaser": "/assets/images/curropinimageForWeb.jpg"
+      },{
+        "title": "Encyclopaedia chapter on PPI prediction published",
+        "excerpt":"   Anna, Sun and Franca contributed a chapter on the prediction of protein-protein interactions in the Encyclopedia of Bioinformatics and Computational Biology. This is now accessible online: Link here.   Congratulations to Anna, Sun and Franca!   ","categories": [],
+        "tags": [],
+        "url": "/encyclopaedia/",
+        "teaser": "/assets/images/Kaleidoscope_PPI_prediction.jpg"
+      },{
+        "title": "HipSci symposium on 22nd March 2018",
+        "excerpt":"   The HipSci consortium will hold its symposium at Guy’s on 22nd March 2018. Franca will give a talk on recent SNV analyses of the HipSci data on behalf of the group!   Details &amp; Registration: http://www.hipsci.org/announcements/symposium-22-mar-2018/  ","categories": [],
+        "tags": [],
+        "url": "/hipsci-symposium/",
+        "teaser": "/assets/images/Screen-Shot-2018-02-05-at-16.52.38.png"
+      },{
+        "title": "BioDynamics Conference on 12 & 13 April 2018",
+        "excerpt":"   https://biodynamics2018.co.uk Franca is one of the organisers for the BioDynamics Conference this year to be held on 12-13 April @ Royal College of Physicians, London. She will chair a session on Molecular Modelling and Mechanisms of Diseases. Do check this out!   ","categories": [],
+        "tags": [],
+        "url": "/biodynamics/",
+        "teaser": "/assets/images/Screen-Shot-2018-03-12-at-16.31.57.png"
+      },{
+        "title": "Franca appointed Head of Randall Centre",
+        "excerpt":"Congratulations to Franca appointed to be the new Head of the Randall Centre of Cell and Molecular Biophysics (link to announcement), under the new School of Basic &amp; Medical Biosciences at King’s!   ","categories": [],
+        "tags": [],
+        "url": "/franca-head-randall/",
+        "teaser": null
+      },{
+        "title": "Some new publications from FFlab",
+        "excerpt":"Some recent proud FFlab productions: Our BRepertoire publication has been included in the NAR Web Server issue 2018. Additions of protein-protein interaction loop analyses in AML by Sun and Biophysical investigations of the human PKM2 enzyme by Jamie now on bioRxiv. Our review on genetic variants and protein-protein interactions is...","categories": [],
+        "tags": [],
+        "url": "/new-publications/",
+        "teaser": null
+      },{
+        "title": "Rasha’s project featured on MND materials",
+        "excerpt":"Our post-doc Rasha’s work on analysing protein-protein interaction networks in the context of the gene C9orf72 affected in Amyotrophic lateral sclerosis (ALS) is featured on promotional materials of the charity Motor Neurone Disease Association! This is an exciting collaboration between the Fraternali lab and Dr Jean-Marc Gallo from the Department...","categories": [],
+        "tags": [],
+        "url": "/rasha-mnd/",
+        "teaser": "/assets/images/rashaFF.png"
+      },{
+        "title": "HTCC4 Structural Biology Workshop",
+        "excerpt":"   Franca will be organising a section on Biomolecular simulations at the upcoming Hot Topics in Contemporary Crystallography (HTCC4) workshop at Dubrovnik, Croatia in the coming October. Register now!   See http://htcc4.org for more information.   ","categories": [],
+        "tags": [],
+        "url": "/htcc4/",
+        "teaser": "/assets/images/HTCC4-Promo-Flyer.jpg"
+      },{
+        "title": "New paper on eLife",
+        "excerpt":"   Our lab has just published a new paper on eLife, investigating the allosteric effects of multiple ligands on the PKM2 enzyme – superb experimental and computational investigations + plus a new package AlloHubMat – a PhD-worth piece of work! Congratulations to Jamie!   ","categories": [],
+        "tags": [],
+        "url": "/jamie-elife/",
+        "teaser": "/assets/images/jamie_elife_assembled.png"
+      },{
+        "title": "Symposium on Networks and Machine Learning @ The Crick",
+        "excerpt":"The Systems and Network Medicine Forum is hosting a Symposium on Network Analysis and Machine Learning Applications in Biology, on Monday 3rd February, 09:00am-13:00pm in The Francis Crick Institute (1 Midland Road, London). Invited talks: Prof Santo Fortunato, Director of the IU Networks Science Institute (IUNI), USA Dr. Andela Šarić,...","categories": [],
+        "tags": [],
+        "url": "/crick-network-symposium/",
+        "teaser": "/assets/images/network_test.png"
+      },{
+        "title": "Irene’s work has been featured on The Times!",
+        "excerpt":"   We are very happy that Irene’s work on simulating self-assembly of an antimicrobial nanocapsule has been featured on The Times.   Read the story on King’s website. The paper was published on ACS Nano.   ","categories": [],
+        "tags": [],
+        "url": "/irene-times/",
+        "teaser": "/assets/images/capsule.jpeg"
+      }]
